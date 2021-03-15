d7aef3433aa22a4fc00512bc5e55c294c899536b
IvoJr <maciel.jr95@gmail.com>
2021-03-15 08:28:06 -0300

    virtual field in scheduling

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -16,7 +16,7 @@ class AppointmentController {
         const appointments = await Appointment.findAll({
             where: { user_id: request.userId, canceled_at: null },
             order: ['date'],
-            attributes: ['id', 'date'],
+            attributes: ['id', 'date', 'past'],
             limit: 20,
             offset: (page - 1) * 20,
             include: [
